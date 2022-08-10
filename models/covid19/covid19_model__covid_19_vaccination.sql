bf2b2a1448afcafedf45dc164de4ab5987251314
DCharles01 <dcharles@kippnyccp.org>
2022-08-10 01:12:28 -0400

    Added a new model
    New model joins covid 19 vaccinations and zip code information

--- models/covid19/covid19_model__covid_19_vaccination.sql
+++ models/covid19/covid19_model__covid_19_vaccination.sql
@@ -1,7 +1,8 @@
 with covid_19_vaccination_data as (
-    select * from {{ source(covid19, covid19_vaccination_data)}}
+    select * from {{ source('covid19', 'covid19_vaccination_data')}}
 ),
 
+-- always use final when using dbt
 final as (
     select * from covid_19_vaccination_data
 )
